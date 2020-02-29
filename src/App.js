e67370eec9c4089fa05add367bc7308e7f0d85aa
John Mark Goodson <john.mark.hat@gmail.com>
2020-02-28 19:54:31 -0500

    building basic text input

--- src/App.js
+++ src/App.js
@@ -2,23 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const update = ["hi", "hello"]; 
+console.log(update)
+
+function HandleClick () {
+  let testing = document.getElementById("inputter")
+  console.log(testing)
+
+}
+
+function Updates (props) {
+  return (
+    <div> {props.list} </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React now
-        </a>
-      </header>
+      <h1> Hello There </h1>
+      <div className="updates"> </div>
+      <Updates list={update}> </Updates>
+      <input type="text" id="inputter"></input> 
+      <button onClick={HandleClick()}> hello </button> 
     </div>
   );
 }
